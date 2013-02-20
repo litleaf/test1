e5aa670caf1122017af9dfcab34a4bb9e29f982d
flash2cocos2dx <litleaf@gmail.com>
2013-02-21 02:03:26 +0800

    test1

---
+++ resources-html5.js
@@ -0,0 +1,11 @@
+var ccb_resources = [
+    {type:'image', src:"ccbResources/ccbParticleStars.png"},
+    {type:'image', src:"clickme-down.png"},
+    {type:'image', src:"ccbResources/ccbParticleSnow.png"},
+    {type:'image', src:"clickme.png"},
+    {type:'image', src:"ccbResources/ccbDefaultImages.png"},
+    {type:'image', src:"ccbResources/ccbParticleFire.png"},
+    {type:'ccbi', src:"MainScene.ccbi"},
+    {type:'plist', src:"ccbResources/ccbDefaultImages.plist"},
+    {type:'image', src:"ccbResources/ccbParticleSmoke.png"},
+];
